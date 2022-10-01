7e48affb34f4ee06a92bc63969d1d3355dda6331
TowhidKashem <tkashem@gmail.com>
2022-10-01 11:31:15 -0500

    work

--- env/lib/xml2Conf.sh
+++
@@ -1,8 +0,0 @@
-#
-# Configuration file for using the XML library in GNOME applications
-#
-XML2_LIBDIR="-L/Users/tkashem/Desktop/github_repos/machine-learning/env/lib"
-XML2_LIBS="-lxml2 -L/Users/tkashem/Desktop/github_repos/machine-learning/env/lib -lz -L/Users/tkashem/Desktop/github_repos/machine-learning/env/lib -llzma -lpthread -L/Users/tkashem/Desktop/github_repos/machine-learning/env/lib -liconv -L/Users/tkashem/Desktop/github_repos/machine-learning/env/lib -licui18n -licuuc -licudata -lm "
-XML2_INCLUDEDIR="-I/Users/tkashem/Desktop/github_repos/machine-learning/env/include/libxml2 -I/Users/tkashem/Desktop/github_repos/machine-learning/env/include"
-MODULE_VERSION="xml2-2.9.14"
-
--- env/lib/xsltConf.sh
+++
@@ -1,8 +0,0 @@
-#
-# Configuration file for using the xslt library
-#
-XSLT_LIBDIR="-L/Users/tkashem/Desktop/github_repos/machine-learning/env/lib"
-XSLT_LIBS="-lxslt -L/Users/tkashem/Desktop/github_repos/machine-learning/env/lib -lxml2 -L/Users/tkashem/Desktop/github_repos/machine-learning/env/lib -L/Users/tkashem/Desktop/github_repos/machine-learning/env/lib -lpthread -L/Users/tkashem/Desktop/github_repos/machine-learning/env/lib -liconv -L/Users/tkashem/Desktop/github_repos/machine-learning/env/lib -licui18n -licuuc -licudata -lm "
-XSLT_PRIVATE_LIBS=""
-XSLT_INCLUDEDIR="-I/Users/tkashem/Desktop/github_repos/machine-learning/env/include"
-MODULE_VERSION="xslt-1.1.35"
